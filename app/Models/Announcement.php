7698dd74cc4a2eab6f0d5d77a4378123f3c38fde
francesco veroli <verolifrancesco@gmail.com>
2021-06-22 11:47:24 +0200

    controller y ruta index nav auth vistas login register

---
+++ app/Models/Announcement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Announcement;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Announcement extends Model
+{
+    use HasFactory;
+}
