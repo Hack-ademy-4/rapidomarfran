ecd20db0d7ce1b20f549179b4c294bf88d4f0018
Maria Ibañez <mirtraffik@gmail.com>
2021-06-29 10:48:29 +0200

    prueba

---
+++ resources/js/announcementImages.js
@@ -0,0 +1,21 @@
+
+import Dropzone from "dropzone";
+Dropzone.autoDiscover = false;
+ 
+(function() {
+    
+    if(document.getElementById("drophere")){
+         
+    let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+    let uniqueSecret = document.querySelector("input[name='uniqueSecret']").getAttribute('value')
+    
+      let myDropzone = new Dropzone('#drophere', {
+              url: '/announcement/images/upload',
+              params: {
+                  _token: csrfToken,
+                  uniqueSecret: uniqueSecret
+              }
+          });
+       }
+    })();
+        
